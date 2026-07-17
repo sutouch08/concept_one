22be88680d04cf78f1ab12a41408081c6db86bf3
sutouch08 <sutouch08@gmail.com>
2026-07-17 12:40:23 +0700

    fix invoice no vat

--- application/views/order_invoice/invoice_add.php
+++ application/views/order_invoice/invoice_add.php
@@ -8,14 +8,14 @@
 	</div>
 	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 padding-5">
 		<p class="pull-right top-p">
-      <button type="button" class="btn btn-sm btn-warning btn-top" onclick="goBack()"><i class="fa fa-arrow-left"></i> Back</button>
-    <?php if($this->pm->can_add) : ?>
-      <button type="button" class="btn btn-sm btn-success btn-top" onclick="showCustomerModal()">ข้อมูลลูกค้า</button>
-    <?php endif; ?>
+			<button type="button" class="btn btn-sm btn-warning btn-top" onclick="goBack()"><i class="fa fa-arrow-left"></i> Back</button>
+			<?php if ($this->pm->can_add) : ?>
+				<button type="button" class="btn btn-sm btn-success btn-top" onclick="showCustomerModal()">ข้อมูลลูกค้า</button>
+			<?php endif; ?>
 		</p>
 	</div>
 </div><!-- End Row -->
-<hr class="hidden-xs"/>
+<hr class="hidden-xs" />
 <div class="row header-row hidden-xs">
 	<div class="col-lg-1-harf col-md-1-harf col-sm-1-harf padding-5">
 		<label>เลขที่</label>
@@ -40,10 +40,10 @@
 		<input type="hidden" id="tax-status" value="">
 	</div>
 	<div class="col-lg-1 col-md-1-harf col-sm-1-harf col-xs-6 padding-5">
-    <label>วันที่</label>
-    <input type="text" class="form-control input-sm text-center h" name="date" id="date" style="position:relative; z-index:10;" value="<?php echo date('d-m-Y'); ?>" required readonly />
-  </div>
-	<div class="divider-hidden visible-xs">		</div>
+		<label>วันที่</label>
+		<input type="text" class="form-control input-sm text-center h" name="date" id="date" style="position:relative; z-index:10;" value="<?php echo date('d-m-Y'); ?>" required readonly />
+	</div>
+	<div class="divider-hidden visible-xs"> </div>
 	<div class="col-lg-1-harf col-md-1-harf col-sm-1-harf col-xs-6 padding-5">
 		<label>รหัสลูกค้า</label>
 		<input type="text" class="form-control input-sm text-center h" maxlength="15" id="customer-code" value="" />
@@ -84,7 +84,7 @@
 
 	<div class="col-lg-4-harf col-md-4-harf col-sm-6-harf col-xs-12 padding-5">
 		<label>ที่อยู่</label>
-		<input type="text" class="form-control input-sm h" maxlength="254"id="address" value="" />
+		<input type="text" class="form-control input-sm h" maxlength="254" id="address" value="" />
 	</div>
 
 	<div class="col-lg-2 col-md-2 col-sm-1-harf col-xs-6 padding-5">
@@ -106,45 +106,45 @@
 </div>
 
 
-<hr class="hidden-xs margin-top-15 margin-bottom-15"/>
+<hr class="hidden-xs margin-top-15 margin-bottom-15" />
 
 <div class="row hidden-xs">
 	<div class="col-lg-1-harf col-md-2 col-sm-3 padding-5">
 		<select class="form-control input-sm h" id="ref-type" onchange="toggleRefType()">
 			<option value="">ค้นเอกสาร</option>
 			<option value="POS">POS</option>
-			<option value="WO">ออเดอร์</option>			
+			<option value="WO">ออเดอร์</option>
 		</select>
 	</div>
 	<div class="col-lg-2 col-md-2 col-sm-3 padding-5">
-    <input type="text" class="form-control input-sm text-center" id="bill-code" placeholder="ค้นบิลขาย" />
+		<input type="text" class="form-control input-sm text-center" id="bill-code" placeholder="ค้นบิลขาย" />
 		<input type="hidden" id="billCode" />
-  </div>
-  <div class="col-lg-1 col-md-1-harf col-sm-1-harf padding-5">
-    <button type="button" class="btn btn-xs btn-primary btn-block" id="btn-submit-bill" onclick="getOrderDetails()">Submit</button>
+	</div>
+	<div class="col-lg-1 col-md-1-harf col-sm-1-harf padding-5">
+		<button type="button" class="btn btn-xs btn-primary btn-block" id="btn-submit-bill" onclick="getOrderDetails()">Submit</button>
 		<button type="button" class="btn btn-xs btn-warning btn-block hide" id="btn-change-bill" onclick="changeBill()">Change</button>
-  </div>
+	</div>
 </div>
-<hr class="padding-5 margin-top-15 margin-bottom-15 hidden-xs"/>
+<hr class="padding-5 margin-top-15 margin-bottom-15 hidden-xs" />
 
 
 <div class="row hidden-xs">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-0 margin-bottom-15">
 		<div class="tabable">
 			<ul class="nav nav-tabs" role="tablist">
-        <li class="active">
-        	<a href="#items-list" id="item-tab" aria-expanded="true" aria-controls="items-list" role="tab" data-toggle="tab">รายการสินค้า</a>
-        </li>
-      	<li>
-          <a href="#down-payment" id="down-tab" aria-expanded="false" aria-controls="down-payment" role="tab" data-toggle="tab">เงินมัดจำ</a>
-        </li>
-      </ul>
+				<li class="active">
+					<a href="#items-list" id="item-tab" aria-expanded="true" aria-controls="items-list" role="tab" data-toggle="tab">รายการสินค้า</a>
+				</li>
+				<li>
+					<a href="#down-payment" id="down-tab" aria-expanded="false" aria-controls="down-payment" role="tab" data-toggle="tab">เงินมัดจำ</a>
+				</li>
+			</ul>
 
 			<!-- Tab panes -->
-      <div class="tab-content" style="margin:0px; padding:0px; border:none;">
+			<div class="tab-content" style="margin:0px; padding:0px; border:none;">
 				<div role="tabpanel" class="tab-pane active" id="items-list">
 					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-0 border-1 table-responsive"
-					style="height:300px; overflow:auto; border-top:solid 1px #ccc;">
+						style="height:300px; overflow:auto; border-top:solid 1px #ccc;">
 						<table class="table table-bordered tableFixHead" style="min-width:1000px; margin-bottom:20px;">
 							<thead>
 								<tr>
@@ -166,8 +166,8 @@
 
 				<div role="tabpanel" class="tab-pane fade" id="down-payment">
 					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-0 border-1 table-responsive"
-					id="down-payment-table"
-					style="height:300px; overflow:auto; border-top:solid 1px #ccc;">
+						id="down-payment-table"
+						style="height:300px; overflow:auto; border-top:solid 1px #ccc;">
 						<table class="table table-bordered tableFixHead" style="margin-bottom:20px;">
 							<thead>
 								<tr>
@@ -208,25 +208,25 @@
 				</div>
 			</div>
 
-			<div class="form-group" >
+			<div class="form-group">
 				<label class="col-lg-3 col-md-3 col-sm-3 control-label no-padding-right">Owner</label>
 				<div class="col-lg-5 col-md-6 col-sm-6 col-xs-12">
 					<input type="text" class="form-control input-sm" id="owner" value="" disabled>
 				</div>
 			</div>
 
-			<div class="form-group" >
+			<div class="form-group">
 				<label class="col-lg-3 col-md-3 col-sm-3 col-xs-12 control-label no-padding-right">Remark</label>
 				<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-					<textarea id="remark" maxlength="254" rows="3" class="form-control" ></textarea>
+					<textarea id="remark" maxlength="254" rows="3" class="form-control"></textarea>
 				</div>
 			</div>
 
 		</div>
 	</div>
 	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 padding-5">
-		<div class="form-horizontal" >
-			<div class="form-group" >
+		<div class="form-horizontal">
+			<div class="form-group">
 				<label class="col-lg-3 col-md-2-harf hidden-sm control-label no-padding-right">จำนวนรวม</label>
 				<label class="col-sm-2 visible-sm control-label no-padding-right">จำนวน</label>
 				<div class="col-lg-2 col-md-2-harf col-sm-3 col-xs-6 padding-5">
@@ -238,45 +238,45 @@
 					<input type="text" class="form-control input-sm text-right" id="total-amount" value="" disabled>
 				</div>
 			</div>
-			<div class="form-group" >
+			<div class="form-group">
 				<label class="col-lg-5-harf col-md-5 col-sm-4 col-xs-6 control-label no-padding-right">ส่วนลด</label>
 				<div class="col-lg-2-harf col-md-3 col-sm-3 col-xs-6 padding-5">
 					<span class="input-icon input-icon-right">
-						<input type="number" id="bill-disc-percent" class="form-control input-sm" value="" disabled/>
+						<input type="number" id="bill-disc-percent" class="form-control input-sm" value="" disabled />
 						<i class="ace-icon fa fa-percent"></i>
 					</span>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-5 col-xs-6 padding-5 last">
-					<input type="text" class="form-control input-sm text-right" id="bill-disc-amount" value=""disabled >
+					<input type="text" class="form-control input-sm text-right" id="bill-disc-amount" value="" disabled>
 					<input type="hidden" id="total-after-disc" value="0" />
 				</div>
 			</div>
 
-			<div class="form-group" id="bill-wht" >
-        <label class="col-lg-5-harf col-md-5 col-sm-4 col-xs-6 control-label no-padding-right">หัก ณ ที่จ่าย</label>
-        <div class="col-lg-2-harf col-md-3 col-sm-3 col-xs-6 padding-5">
-          <span class="input-icon input-icon-right">
-          <input type="number" id="whtPrcnt" class="form-control input-sm" onchange="recalTotal()" value="" disabled/>
-          <i class="ace-icon fa fa-percent"></i>
-          </span>
-        </div>
-        <div class="col-lg-4 col-md-4 col-sm-5 col-xs-6 padding-5 last">
-          <input type="hidden" id="wht-amount" value="" />
-          <input type="text" id="wht-amount-label" class="form-control input-sm text-right" value="" disabled />
-        </div>
-      </div>
+			<div class="form-group" id="bill-wht">
+				<label class="col-lg-5-harf col-md-5 col-sm-4 col-xs-6 control-label no-padding-right">หัก ณ ที่จ่าย</label>
+				<div class="col-lg-2-harf col-md-3 col-sm-3 col-xs-6 padding-5">
+					<span class="input-icon input-icon-right">
+						<input type="number" id="whtPrcnt" class="form-control input-sm" onchange="recalTotal()" value="" disabled />
+						<i class="ace-icon fa fa-percent"></i>
+					</span>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-5 col-xs-6 padding-5 last">
+					<input type="hidden" id="wht-amount" value="" />
+					<input type="text" id="wht-amount-label" class="form-control input-sm text-right" value="" disabled />
+				</div>
+			</div>
 
-			<div class="form-group" id="bill-vat" >
+			<div class="form-group" id="bill-vat">
 				<label class="col-lg-6-harf col-md-6 col-sm-5 col-xs-6 control-label no-padding-right">VAT</label>
 				<div class="col-lg-1-harf col-md-2 col-sm-2 padding-5">
-					<input type="text" id="vat-type-label" class="form-control input-sm text-center" value="Include" disabled/>
+					<input type="text" id="vat-type-label" class="form-control input-sm text-center" value="Include" disabled />
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-5 col-xs-6 padding-5 last">
 					<input type="text" id="vat-total" class="form-control input-sm text-right" value="" disabled />
 				</div>
 			</div>
 
-			<div class="form-group" >
+			<div class="form-group">
 				<label class="col-lg-8 col-md-8 col-sm-7 col-xs-6 control-label no-padding-right">รวมทั้งสิ้น</label>
 				<div class="col-lg-4 col-md-4 col-sm-5 col-xs-6 padding-5 last">
 					<input type="text" class="form-control input-sm text-right" id="doc-total" value="" disabled>
@@ -346,16 +346,16 @@
 					data-zonecode="{{zone_code}}"
 					data-linetext="{{line_text}}"
 					data-iscount="{{is_count}}"
-					value="{{qty_label}}" readonly	/>
+					value="{{qty_label}}" readonly />
 			</td>
 			<td class="text-right">
-				<input type="text" class="form-control input-xs text-center" style="border:0px;" id="price-{{id}}" value="{{price_label}}" readonly	/>
+				<input type="text" class="form-control input-xs text-center" style="border:0px;" id="price-{{id}}" value="{{price_label}}" readonly />
 			</td>
 			<td class="text-right">
-				<input type="text" class="form-control input-xs text-center" style="border:0px;"	value="{{discount_label}}" readonly	/>
+				<input type="text" class="form-control input-xs text-center" style="border:0px;" value="{{discount_label}}" readonly />
 			</td>
 			<td class="text-right">
-				<input type="text" class="form-control input-xs text-right"	style="border:0px;"	id="total-{{id}}"	value="{{line_total_label}}" readonly	/>
+				<input type="text" class="form-control input-xs text-right" style="border:0px;" id="total-{{id}}" value="{{line_total_label}}" readonly />
 			</td>
 		</tr>
 	{{/each}}
@@ -393,16 +393,16 @@
 					data-zonecode="{{zone_code}}"
 					data-linetext="{{line_text}}"
 					data-iscount="{{is_count}}"
-					value="{{qty_label}}" readonly	/>
+					value="{{qty_label}}" readonly />
 			</td>
 			<td class="text-right">
-				<input type="text" class="form-control input-xs text-center" style="border:0px;" id="price-{{id}}" value="{{price_label}}" readonly	/>
+				<input type="text" class="form-control input-xs text-center" style="border:0px;" id="price-{{id}}" value="{{price_label}}" readonly />
 			</td>
 			<td class="text-right">
-				<input type="text" class="form-control input-xs text-center" style="border:0px;"	value="{{discount_label}}" readonly	/>
+				<input type="text" class="form-control input-xs text-center" style="border:0px;" value="{{discount_label}}" readonly />
 			</td>
 			<td class="text-right">
-				<input type="text" class="form-control input-xs text-right"	style="border:0px;"	id="total-{{id}}"	value="{{line_total_label}}" readonly	/>
+				<input type="text" class="form-control input-xs text-right" style="border:0px;" id="total-{{id}}" value="{{line_total_label}}" readonly />
 			</td>
 		</tr>
 	{{/each}}
@@ -484,9 +484,12 @@
 </script>
 
 <div class="row visible-xs">
-	<div class="col-xs-12"><h1 class="text-center">Not support mobile</h1></div>
+	<div class="col-xs-12">
+		<h1 class="text-center">Not support mobile</h1>
+	</div>
 </div>
 
 <script src="<?php echo base_url(); ?>scripts/order_invoice/order_invoice.js?v=<?php echo date('Ymd'); ?>"></script>
 <script src="<?php echo base_url(); ?>scripts/order_invoice/order_invoice_bill.js?v=<?php echo date('Ymd'); ?>"></script>
+<script src="<?php echo base_url(); ?>scripts/print/print_invoice.js?v=<?php echo date('Ymd'); ?>"></script>
 <?php $this->load->view('include/footer'); ?>
